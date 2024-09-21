5363126541f34b992dd9b7120729b439c971d2b7
Maxime <maximechagnon00@gmail.com>
2024-09-21 21:49:17 +0200

    feat: stars

--- scripts/MoonSprite.cs
+++ scripts/MoonSprite.cs
@@ -4,13 +4,11 @@ using System;
 public partial class MoonSprite : Sprite2D
 {
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
+	public override void _Ready() {
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+	public override void _Process(double delta) {
 		// The moon sprite will grow and shrink in size over time.
 		float scale = 1.5f + 0.1f * (float) Math.Sin(Time.GetTicksMsec() / 1000.0);
 		this.Scale = new Vector2(scale, scale);
--- scripts/Root.cs
+++ scripts/Root.cs
@@ -6,30 +6,62 @@ public partial class Root : Control
 	private Label stardustCounter;
 	private int stardust = 0;
 
+	private Label starCounter;
+	private int stars = 0;
+	private Button starBuyButton;
+	private int starPrice = 10;
+
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		stardustCounter = GetNode<Label>("StardustCounter");
+	public override void _Ready() {
+		stardustCounter = GetNode<Label>("Resources/StardustCounter");
 		UpdateStardustCounter();
+
+		starCounter = GetNode<Label>("Shop/Star/StarCounter");
+		UpdateStarCounter();
+		starBuyButton = GetNode<Button>("Shop/Star/StarBuyButton");
+		UpdateStarPrice();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+	public override void _Process(double delta) {
 	}
 
-	public void AddStardust(int amount)
-	{
+	public void AddStardust(int amount) {
 		stardust += amount;
 		UpdateStardustCounter();
 	}
 
-	public void UpdateStardustCounter()
-	{
+	public void UpdateStardustCounter() {
 		stardustCounter.Text = "Stardust: " + stardust;
+	}
+
+	public void UpdateStarCounter() {
+        string starPlural = stars == 1 ? "" : "s";
+		starCounter.Text = stars + " Star" + starPlural;
+	}
+
+	public void UpdateStarPrice() {
+		// The base price is 10 stardust but increases by 10% for each star bought.
+		starPrice += (int) (10 * stars * 0.1f);
+		starBuyButton.Text = "Buy Star (" + starPrice + " Stardust)";
 	}
 
 	public void OnMoonButtonPressed() {
 		AddStardust(1);
+	}
+
+	public void OnStarBuyButtonPressed() {
+		if (stardust >= starPrice) {
+			AddStardust(starPrice * -1);
+			stars++;
+			UpdateStarCounter();
+			UpdateStarPrice();
+
+			// Add a new star sprite to the scene.
+			Sprite2D star = new Sprite2D();
+			GetTree().Root.AddChild(star);
+			star.Texture = GD.Load<Texture2D>("res://assets/medium_star.png");
+			star.Position = new Vector2(100 + 50 * stars, 100);
+		}
 	}
 }
