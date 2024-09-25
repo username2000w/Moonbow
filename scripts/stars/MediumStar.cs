52d7b8f618d4c0c84eccb5d188ec6d5cff670874
Maxime <maximechagnon00@gmail.com>
2024-09-25 11:56:14 +0200

    fea:t stars are movable

--- scripts/stars/MediumStar.cs
+++ scripts/stars/MediumStar.cs
@@ -4,6 +4,9 @@ using System;
 public partial class MediumStar : Control {
 	private Timer timer;
 
+	private bool mouseOver = false;
+	private bool ok = false;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
 		Position = new Vector2(GD.Randf() * GetViewportRect().Size.X, GD.Randf() * GetViewportRect().Size.Y);
@@ -16,12 +19,27 @@ public partial class MediumStar : Control {
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta) {
-
+		if (mouseOver && Input.IsActionJustPressed("click")) {
+			if (ok) {
+				ok = false;
+			} else {
+				ok = true;
+			}
+		}
+		if (ok) Position = GetGlobalMousePosition() - GetChild<Sprite2D>(0).Texture.GetSize() / 2;
 	}
 
 	public void OnTimerTimeout() {
 		if (GetParent() is Root root) {
 			root.AddStardust(1);
 		}
+	}
+
+	public void OnMouseEntered() {
+		mouseOver = true;
+	}
+
+	public void OnMouseExited() {
+		mouseOver = false;
 	}
 }
