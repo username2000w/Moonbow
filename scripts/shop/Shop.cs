c780d33365c7bf212ffeffa08583f598bd847d6e
Maxime <maximechagnon00@gmail.com>
2024-10-01 19:19:35 +0200

    refactor: shop opening

--- scripts/shop/Shop.cs
+++ scripts/shop/Shop.cs
@@ -2,14 +2,8 @@ using Godot;
 using System;
 
 public partial class Shop : VBoxContainer {
-	Button _shopButton;
-
-	public override void _Ready() {
-		this._shopButton = this.GetNode<Button>("../ShopButton");
-	}
 
 	public void OnShopButtonPressed() {
 		this.Visible = !this.Visible;
-		_shopButton.Text = this.Visible ? "Close Shop" : "Open Shop";
 	}
 }
